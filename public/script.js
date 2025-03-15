8dcba0f65088c3a288ac1b908d969e9f54cad336
xapoow <maxmorozov693@gmail.com>
2025-03-15 15:55:44 +0100

    3

---
+++ public/script.js
@@ -0,0 +1,26 @@
+function loadSentences() {
+    fetch('get_sentences.php')
+        .then(response => response.json())
+        .then(data => {
+            const historyDiv = document.getElementById('history');
+            historyDiv.innerHTML = data.map(s => `${s.text}`).join(' ');
+        });
+}
+function checkLogin() {
+    fetch('session_status.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data.logged_in) {
+                document.getElementById('logoutForm').style.display = 'flex';
+                document.getElementById('authForms').style.display = 'none';
+            } else {
+                document.getElementById('logoutForm').style.display = 'none';
+                document.getElementById('authForms').style.display = 'flex';
+            }
+        })
+        .catch(error => console.error('Error fetching session status:', error)); // Add error handling
+}
+window.onload = function() {
+    loadSentences();
+    checkLogin();
+};
